fce2f8f1b4af19dee6f8530cfa56b83f132503c3
Joe Reed <reedj11@mc-n356503.home>
2019-01-24 20:12:41 +0000

    adds skeleton footer

---
+++ components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import "./Footer.scss";
+
+export default () => (
+    <footer>
+        <div className="footer__triangle"></div>
+        <div className="footer__content">
+            <ul className="footer__links">
+                <li>
+                    Github
+                </li>
+                <li>
+                    Twitter
+                </li>
+                <li>
+                    Unsplash
+                </li>
+            </ul>
+        </div>
+    </footer>
+);
