9c6473193057c90ab59324798e7f977aaff79cfe
Joe Reed <reedj11@mc-n356503.home>
2019-02-03 17:10:45 +0000

    Adds tbody to table

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -4,14 +4,16 @@ export default () => (
     <div className="contact">
         <h2 className="contact__header">Contact</h2>
         <table className="contact__table">
-            <tr className="contact__item">
-                <th>email:</th>
-                <td><a className="contact__link" href="mailto:joe.reed.4@gmail.com">joe.reed.4@gmail.com</a></td>
-            </tr>
-            <tr className="contact__item">
-                <th>twitter:</th>
-                <td><a className="contact__link" href="https://www.twitter.com/joerobot">@joerobot</a></td>
-            </tr>
+            <tbody>
+                <tr className="contact__item">
+                    <th>email:</th>
+                    <td><a className="contact__link" href="mailto:joe.reed.4@gmail.com">joe.reed.4@gmail.com</a></td>
+                </tr>
+                <tr className="contact__item">
+                    <th>twitter:</th>
+                    <td><a className="contact__link" href="https://www.twitter.com/joerobot">@joerobot</a></td>
+                </tr>
+            </tbody>
         </table>
     </div>
 );
