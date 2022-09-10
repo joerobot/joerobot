1af3f9134a3363a5de31aa84a8859dfccebfbb38
Joe Reed <jreed@fastly.com>
2022-09-10 18:29:24 +0100

    Updates favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -47,6 +47,27 @@ function MyApp({ Component, pageProps }: any) {
       <Layout type={layoutType}>
         <Head>
           <meta name="viewport" content="width=device-width" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="msapplication-config" content="/browserconfig.xml" />
+          <meta name="msapplication-TileColor" content="#F8F5F0" />
+          <meta name="theme-color" content="#F8F5F0" />
         </Head>
         <Component {...pageProps} />
       </Layout>
